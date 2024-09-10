f5803d20bca43c411a6bd041fada39377ead37bd
PRIYANSHI KASAUDHAN <147182966+Priyanshikasaudhan@users.noreply.github.com>
2024-09-10 17:08:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['SoftWare Engineer!' , 'Fronted developer!', 'Programmer!'],
+    strings: ['SoftWare Engineer!' , 'Frontend developer!', 'Programmer!'],
     typeSpeed:100,
     backSpeed: 100,
     backDelay: 1000,
